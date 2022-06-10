02ea3c7584a5a32b04963b89ad97d8dcc73f02cb
Dawid Byczkowski <35502248+byczkos@users.noreply.github.com>
2022-06-10 11:03:19 +0200

    Update RestController.java

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class RestController {
 
-    private int calculate (int a, int b) {
-        return a*b;
+    private int addition (int a, int b) {
+        return a+b;
     }
 }
