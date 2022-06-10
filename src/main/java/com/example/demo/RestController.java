5b943557265009e5958024c156f8c93df59978d2
Dawid Byczkowski <dbyczkowski@gmail.com>
2022-06-10 11:04:30 +0200

    Change name of method

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class RestController {
 
-    private int calculate (int a, int b) {
-        return a*b;
+    private int subtraction (int a, int b) {
+        return a-b;
     }
 }
